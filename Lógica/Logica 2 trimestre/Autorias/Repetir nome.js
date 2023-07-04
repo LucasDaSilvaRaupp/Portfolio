c40029dbca596681105278f22a15d96561c234bf
LucasDaSilvaRaupp <129192608+LucasDaSilvaRaupp@users.noreply.github.com>
2023-07-04 12:29:13 -0300

    Add files via upload

---
+++ Lógica/Logica 2 trimestre/Autorias/Repetir nome.js
@@ -0,0 +1,15 @@
+/* Bloco do var */
+var nome = "Lucas" // Declarando a variável
+console.log(nome)
+
+var nome = "Raupp" // Recriando a variável
+console.log(nome)
+
+nome = "Lucas Raupp" // Retribuindo valor
+console.log(nome)
+
+/* Bloco do let
+function getNome(){
+
+    let nome = "Lucas";;
+    */
---
+++ Lógica/Logica 2 trimestre/Autorias/vetor = [1, 2, 3].js
@@ -0,0 +1,6 @@
+vetor =  [1, 2, 3]
+function imprimeBonito (numero, i, arr){
+    console.log(`${numero} é o elemento na posição ${i} do vetor ${arr} `)
+    console.log(numero + " é o elemento na posição" + i + "do vetor" + arr)
+}
+vetor.forEach(imprimeBonito)
